f0e28f98636af4c2fb9dd69a7834a4b7e6fc7ac6
Lucas Simão <lucas.simao01@gmail.com>
2023-11-01 00:18:55 -0400

    feat: genreate html with all files

---
+++ internal/server/server.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+	_ "embed"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/lucas-simao/go-gen/internal/templates"
+	"github.com/lucas-simao/go-gen/internal/utils"
+)
+
+type Response struct {
+	Controller string `json:"controller"`
+	UseCase    string `json:"useCase"`
+	Model      string `json:"model"`
+	Repository string `json:"repository"`
+}
+
+//go:embed html/index.html
+var indexHTML string
+
+func NewServer() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", getIndex())
+	mux.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("internal/server/css"))))
+
+	return mux
+}
+
+func getIndex() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.New("index").Parse(indexHTML)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		projectName := r.URL.Query().Get("projectName")
+		serviceName := r.URL.Query().Get("serviceName")
+		jsonParsed := r.URL.Query().Get("json")
+
+		t.Execute(w, func() map[string]string {
+			if len(projectName) > 0 && len(serviceName) > 0 && len(jsonParsed) > 0 {
+				model := templates.GenerateModel(serviceName, jsonParsed)
+
+				return map[string]string{
+					"ModelPath":      fmt.Sprintf("/%s/src/domain/models/%s.go", utils.ToLower(projectName), utils.ToLower(serviceName)),
+					"Model":          model,
+					"ControllerPath": fmt.Sprintf("/%s/src/application/controllers/%s_controller.go", utils.ToLower(projectName), utils.ToLower(serviceName)),
+					"Controller":     templates.GenerateController(serviceName, projectName),
+					"UseCasePath":    fmt.Sprintf("/%s/src/domain/usecases/%s_usecases.go", utils.ToLower(projectName), utils.ToLower(serviceName)),
+					"UseCase":        templates.GenerateUsecases(serviceName, projectName),
+					"RepositoryPath": fmt.Sprintf("/%s/src/infra/repositories/%s_repository.go", utils.ToLower(projectName), utils.ToLower(serviceName)),
+					"Repository":     templates.GenerateRepository(model, serviceName, projectName),
+				}
+			}
+			return map[string]string{}
+		}())
+	}
+}
--- main.go
+++ main.go
@@ -1,58 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
-	"github.com/lucas-simao/go-gen/internal/templates"
+	"github.com/lucas-simao/go-gen/internal/server"
 )
 
-type Request struct {
-	ServiceName string `json:"serviceName"`
-	ProjectName string `json:"projectName"`
-	Json        string `json:"json"`
-}
-
-type Response struct {
-	Controller string `json:"controller"`
-	UseCase    string `json:"useCase"`
-	Model      string `json:"model"`
-	Repository string `json:"repository"`
-}
-
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var p *Request
-
-		err := json.NewDecoder(r.Body).Decode(&p)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(err.Error()))
-			return
-		}
-
-		model := templates.GenerateModel(p.ServiceName, p.Json)
-
-		var resp = Response{
-			Controller: templates.GenerateController(p.ServiceName, p.ProjectName),
-			UseCase:    templates.GenerateUsecases(p.ServiceName, p.ProjectName),
-			Model:      model,
-			Repository: templates.GenerateRepository(model, p.ServiceName, p.ProjectName),
-		}
-
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(http.StatusCreated)
-
-		err = json.NewEncoder(w).Encode(resp)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(err.Error()))
-			return
-		}
-	})
-
-	if err := http.ListenAndServe(":9000", nil); err != nil {
+	if err := http.ListenAndServe(":8080", server.NewServer()); err != nil {
 		log.Panic(err)
 	}
 }
